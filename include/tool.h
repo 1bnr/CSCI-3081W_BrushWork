30e7cc1843c3f195c9acbec36830fe4352ac27e7
Isaac <schw1643@umn.edu>
2016-10-05 14:57:57 -0500

    Created the template for the tool.h class.

---
+++ include/tool.h
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Name            : tool.h
+ * Project         : BrushWork
+ * Module          : utils
+ * Description     : Header file for Tools class.
+ * Copyright       : 2016 CSCI3081W Team 0x07 All rights reserved.
+ * Creation Date   : 10/05/16
+ * Original Author : Isaac Schwab
+ *
+ ******************************************************************************/
+
+ #ifndef INCLUDE_TOOL_H_
+ #define INCLUDE_TOOL_H_
+
+ /*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+namespace image_tools {
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @brief This color data class stores color in floating point format.
+ * The Red, Green, Blue, and Alpha channels range from 0.0 to 1.0.
+ */
+class Tool {
+ public:
+
+ private:
+
+};
+}  // namespace image_tools
+
+#endif  // INCLUDE_TOOL_H
