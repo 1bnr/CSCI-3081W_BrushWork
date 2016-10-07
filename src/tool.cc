6eccc724c6d2627b1f8ea2e9a49e72b18c84c355
Isaac <schw1643@umn.edu>
2016-10-07 18:46:19 -0500

    (tool.cc) Added implementation details for the constuctor, and deconstructor. - Defined Draw to test output when called upon. Actual implementation will be in next push.

--- src/tool.cc
+++ src/tool.cc
@@ -13,6 +13,8 @@
  * Includes
  ******************************************************************************/
 #include "include/tool.h"
+#include <cmath>
+#include <iostream>
 
 /*******************************************************************************
  * Namespaces
@@ -22,7 +24,25 @@ namespace image_tools {
 /*******************************************************************************
  * Constructors/Destructors
  ******************************************************************************/
+Tool::Tool() {
+}
 
+Tool::~Tool() {}
+
+ //The Draw fucntion
+void Tool::Draw(int x, int y) {
+	// int rows = 3;
+	// int cols = 3;
+	// int xpos_start = x - rows/2; //set the start pos
+	// int xpos_end = x + rows/2;
+	std::cout << "FUCKING PRINT" << std::endl; 
+	// for(int i = 0; i < rows; i++) {
+	// 	for(int j = 0; j < rows) {
+
+	// 	}
+	// }
+
+}
 
 /*******************************************************************************
  * Member Functions
