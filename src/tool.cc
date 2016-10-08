68faac12677a2a0e7af4c88ffa98aefaa7196ed9
Isaac <schw1643@umn.edu>
2016-10-07 20:36:06 -0500

    (tool.cc) Used a hard coded mask for testing the functionallity, essentially is a pen mask. It also handles setting the mask around the position of the cursor that we were passed in.

--- src/tool.cc
+++ src/tool.cc
@@ -29,18 +29,27 @@ Tool::Tool() {
 
 Tool::~Tool() {}
 
- //The Draw fucntion
-void Tool::Draw(int x, int y) {
-	// int rows = 3;
-	// int cols = 3;
-	// int xpos_start = x - rows/2; //set the start pos
-	// int xpos_end = x + rows/2;
-	std::cout << "FUCKING PRINT" << std::endl; 
-	// for(int i = 0; i < rows; i++) {
-	// 	for(int j = 0; j < rows) {
+ //The Draw fucntion, take in a color object and the pointer to our display buffer
+void Tool::Draw(int x, int y, PixelBuffer *p, ColorData c) {
+	//hard coded in the specs for the pen mask boundries,for testing sake
+	// this is where I will be making more changes
+	int rows = 3;
+	int cols = 3;
+	int xpos_start = x - rows/2; //set the start xpos
+	int xpos_end = x + rows/2;
+	int ypos_start = y - cols/2; 
+	int ypos_end = y + cols/2;
 
-	// 	}
-	// }
+	std::cout << "PRINT" << std::endl; 
+	//loop through the coordinates that the mask will effect and set the points to the color that we want
+	//also note that I'm not yet handling the fall off in the mask, because this is just hard coded in
+	for(int i = xpos_start; i <= xpos_end; i++) {
+		for(int j = ypos_start; j <= ypos_end; j++) {
+			std::cout << "i: " << i << std::endl; 
+			std::cout << "j: " << j << std::endl; 
+			p->set_pixel(i, j, c);  //set the current pixel to the display buffer that we passed in
+		}
+	}
 
 }
 
