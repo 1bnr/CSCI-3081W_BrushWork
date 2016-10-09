ecc341217beb6fcb9a3be6406199386be4362f3f
Abner <holsi006@umn.edu>
2016-10-09 15:24:12 -0500

    tool now uses mask as filter to mix pixels 	modified:   src/tool.cc

--- src/tool.cc
+++ src/tool.cc
@@ -33,34 +33,35 @@ Tool::~Tool() {}
 void Tool::Draw(int x, int y, PixelBuffer *p, ColorData c) {
 	int t_x_ = x;
 	int t_y_ = p->height() - y;
-	
-	//the following variables are setup to handle pizels surround the cursor
-	int rows = mask_rows_;
-	int cols = mask_cols_;
-	int xpos_start = t_x_ - rows/2; //set the start xpos
-	int xpos_end = t_x_ + rows/2;
-	int ypos_start = t_y_ - cols/2; 
-	int ypos_end = t_y_ + cols/2;
+	//hard coded in the specs for the pen mask boundries,for testing sake
+	// this is where I will be making more changes
+//	int rows = (*mask_size_);
+//	int cols = (*mask_size_);
+	int xpos_start = t_x_ - mask_rows_/2; //set the start xpos
+	int xpos_end = t_x_ + mask_rows_/2;
+	int ypos_start = t_y_ - mask_cols_/2; 
+	int ypos_end = t_y_ + mask_cols_/2;
 
-	//indexes for the current position of the mask array
-	int mask_x = 0;
-	int mask_y = 0;
-	//loop through the coordinates that the mask will effect 
-	//and set the points to the color that we want
-	for(int i = xpos_start; i <= xpos_end; i++) {
-		for(int j = ypos_start; j <= ypos_end; j++) {
-			std::cout << "i: " << mask_x << std::endl; 
-			std::cout << "j: " << mask_y << std::endl;
-			std::cout << "Mask Val: " << tool_mask_[mask_x][mask_y]  << std::endl;
-			//don't draw to the buffer if the current mask position is empty
-			if(tool_mask_[mask_x][mask_y] > 0) {
-				p->set_pixel(i, j, c);  //set the current pixel to the display buffer that we passed in
-			}
-			mask_y+=1;
-		}
-		mask_y = 0;
-		mask_x+=1;
-	}
+  // filter the pixel data from the canvas and the tool through the mask, and apply them to the canvas
+  for(int i = 0; i < mask_rows_; i++) {
+    for(int j = 0; j < mask_cols_; j++) {
+      ColorData canvas_pixel = (p->get_pixel(xpos_start + i, ypos_start + j)) * static_cast<float>(1.0 - tool_mask_[i][j]);
+      ColorData tool_pixel = c * (static_cast<float>(tool_mask_[i][j]));
+// print out the mask      
+//std::cout << tool_mask_[i][j] << ":";
+
+      ColorData new_pixel = canvas_pixel + tool_pixel;     
+        //set the current pixel to the display buffer that we passed in
+        p->set_pixel(j + xpos_start, i + ypos_start, new_pixel);  
+
+    }
+// skip to next line of mask for printout
+//std::cout << std::endl;
+
+  }
+// space between filter printouts
+//std::cout << std::endl;
+
 
 }
 
