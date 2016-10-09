7d530ab58ae7011a0f7532f54feb4265cb1b9145
Isaac <schw1643@umn.edu>
2016-10-08 20:53:44 -0500

    (tool.cc) Reworked draw so we now opertate based off the tool_mask_ member variable. Still need to implement how to handle different alpha values within the mask array. For now we simply just handle values of 1 and 0.

--- src/tool.cc
+++ src/tool.cc
@@ -29,28 +29,37 @@ Tool::Tool() {
 
 Tool::~Tool() {}
 
- //The Draw fucntion, take in a color object and the pointer to our display buffer
+ //The Draw fucntion, take in a color object & the pointer to display buffer
 void Tool::Draw(int x, int y, PixelBuffer *p, ColorData c) {
 	int t_x_ = x;
 	int t_y_ = p->height() - y;
-	//hard coded in the specs for the pen mask boundries,for testing sake
-	// this is where I will be making more changes
-	int rows = 3;
-	int cols = 3;
+	
+	//the following variables are setup to handle pizels surround the cursor
+	int rows = mask_rows_;
+	int cols = mask_cols_;
 	int xpos_start = t_x_ - rows/2; //set the start xpos
 	int xpos_end = t_x_ + rows/2;
 	int ypos_start = t_y_ - cols/2; 
 	int ypos_end = t_y_ + cols/2;
 
-	std::cout << "PRINT" << std::endl; 
-	//loop through the coordinates that the mask will effect and set the points to the color that we want
-	//also note that I'm not yet handling the fall off in the mask, because this is just hard coded in
+	//indexes for the current position of the mask array
+	int mask_x = 0;
+	int mask_y = 0;
+	//loop through the coordinates that the mask will effect 
+	//and set the points to the color that we want
 	for(int i = xpos_start; i <= xpos_end; i++) {
 		for(int j = ypos_start; j <= ypos_end; j++) {
-			std::cout << "i: " << i << std::endl; 
-			std::cout << "j: " << j << std::endl; 
-			p->set_pixel(i, j, c);  //set the current pixel to the display buffer that we passed in
+			std::cout << "i: " << mask_x << std::endl; 
+			std::cout << "j: " << mask_y << std::endl;
+			std::cout << "Mask Val: " << tool_mask_[mask_x][mask_y]  << std::endl;
+			//don't draw to the buffer if the current mask position is empty
+			if(tool_mask_[mask_x][mask_y] > 0) {
+				p->set_pixel(i, j, c);  //set the current pixel to the display buffer that we passed in
+			}
+			mask_y+=1;
 		}
+		mask_y = 0;
+		mask_x+=1;
 	}
 
 }
