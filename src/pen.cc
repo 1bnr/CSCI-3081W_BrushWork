b078d30afb546b2ccb6d1e6789791703d17617e9
Abner <holsi006@umn.edu>
2016-10-12 13:58:42 -0500

    changed pen to generate mask 	modified:   src/pen.cc

--- src/pen.cc
+++ src/pen.cc
@@ -12,6 +12,7 @@
 /*******************************************************************************
  * Includes
  ******************************************************************************/
+#include <cmath>
 #include "include/pen.h"
 
 /*******************************************************************************
@@ -26,23 +27,29 @@ Pen::Pen() : Tool() {
   mask_rows_ = 3;
   mask_cols_ = 3;
 
-  // Set up the tool_mask_
-  tool_mask_.resize(mask_rows_);
-  for (int i = 0; i < mask_rows_; ++i)
-    tool_mask_[i].resize(mask_cols_);
+   // Set up the tool_mask_
+   tool_mask_.resize(mask_rows_);
+   for (int i = 0; i < mask_rows_; ++i)
+     tool_mask_[i].resize(mask_cols_);
 
-  //Initialize the mask
-  tool_mask_[0] = {0.0, 1, 0.0};
-  tool_mask_[1] = {1, 1, 1};
-  tool_mask_[2] = {0.0, 1, 0.0};
+  int center_ = mask_cols_ / 2;
+  for (int y = 0; y < mask_rows_; y++) {
+    tool_mask_[y].resize(mask_cols_);
+    for (int x = 0; x < mask_cols_; x++) {
+      int xy_pos = pow(x - center_, 2) + pow(y - center_, 2);
+      // if inside circle, set filter to 1; apply color opaque
+      if ( xy_pos <= pow(center_, 2) )
+        tool_mask_[x][y] = 1;
+      else  // outside circle, set filter value to 0; block color application
+        tool_mask_[x][y] = 0;
+      }
+   }
 }
-
 Pen::~Pen() {
 }
 
 /*******************************************************************************
  * Member Functions
  ******************************************************************************/
-
 
 }  // namespace image_tools
