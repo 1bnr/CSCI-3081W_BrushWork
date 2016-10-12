6ccad739172ec5e65b1a026bf884e691d4a9fb53
Isaac <schw1643@umn.edu>
2016-10-12 16:31:42 -0500

    Added in border detection and handling within the draw method. This handles the case in which the mask attempts to draw outide the appliccation border. Also fixed how we were drawing the masks in the incorrect orientation.

--- src/tool.cc
+++ src/tool.cc
@@ -33,29 +33,39 @@ Tool::~Tool() {}
 void Tool::Draw(int x, int y, PixelBuffer *p, ColorData c) {
   // adjust the y mouse-parameter to the canvas orientation
   y = p->height() - y;
+  //borders for the application
+  int window_width = p->width();
+  int window_height = p->height();
   // set the start and end positions of the draw mask
-  int xpos_start = x - mask_rows_/2;
-  int xpos_end = x + mask_rows_/2;
-  int ypos_start = y - mask_cols_/2;
-  int ypos_end = y + mask_cols_/2;
+  int xpos_start = x - mask_cols_/2;
+  int ypos_start = y - mask_rows_/2;
 
   // filter the pixel data from the canvas and the tool through the mask, and
   // apply them to the canvas
-  for (int p_x = 0; p_x < mask_rows_; p_x++) {
-    for (int p_y = 0; p_y < mask_cols_; p_y++) {
-      ColorData canvas_pixel = p->get_pixel(xpos_start + p_x, ypos_start + p_y);
-      float intensity = static_cast<float>(tool_mask_[p_x][p_y]);
-      if (uses_luminance_)
-        intensity *= canvas_pixel.luminance();
-      float canvas_filter = 1.0 - tool_mask_[p_x][p_y];
-      float mask_filter = static_cast<float>(tool_mask_[p_x][p_y]);
-
-      // print out the mask; for debugging
-      // std::cout << tool_mask_[p_x][p_y] << ":";
-
-      ColorData new_pixel = (canvas_pixel * canvas_filter) + (c * mask_filter);
+  for (int p_y = 0; p_y < mask_rows_; p_y++) {
+    for (int p_x = 0; p_x < mask_cols_; p_x++) {
+      
+      
       // set the current pixel to the display buffer that we passed in
-      p->set_pixel(p_x + xpos_start, p_y + ypos_start, new_pixel);
+      // Check that we do not draw outside the border of the application
+      if((p_x + xpos_start) >= 0 && (p_x + xpos_start) < window_width &&
+        (p_y + ypos_start) >= 0 && (p_y + ypos_start) < window_height) {
+        ColorData canvas_pixel = p->get_pixel(xpos_start + p_x, ypos_start + p_y);
+        float intensity = static_cast<float>(tool_mask_[p_y][p_x]);
+        if (uses_luminance_)
+          intensity *= canvas_pixel.luminance();
+        float canvas_filter = 1.0 - tool_mask_[p_y][p_x];
+        float mask_filter = static_cast<float>(tool_mask_[p_y][p_x]);
+
+        // print out the mask; for debugging
+        // std::cout << tool_mask_[p_x][p_y] << ":";
+        ColorData new_pixel = (canvas_pixel * canvas_filter) + (c * mask_filter);
+        p->set_pixel(p_x + xpos_start, p_y + ypos_start, new_pixel);
+      }
+        
+        
+
+      
     }
     // skip to next line of mask for printout
     // std::cout << std::endl;
