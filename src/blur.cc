c78f2e6cf64bccd91e8de0d7672c753e7b1a80ba
Isaac <schw1643@umn.edu>
2016-11-17 10:06:50 -0600

    (blur filter) -finished up the blur filter. Now works correctly for all input values -kernel values are generate based off input -kernel is also generated for each pixel to prevent black border

--- src/blur.cc
+++ src/blur.cc
@@ -31,36 +31,21 @@ Blur::~Blur() {}
  * Member Functions
  ******************************************************************************/
  void Blur::apply_filter(PixelBuffer* p, float blur_amount){
+   // PixelBuffer height and width
    int x = p->width();
    int y = p->height();
+   // Calculate an odd numbered bound based off the input
    int bounds = static_cast<int>(floor(blur_amount));
    if (bounds % 2 == 0) {
     bounds++;
    }
-   std::cout << "bounds: " << bounds << std::endl;
-   float blur_frac = 1.0 / ((2*bounds)-1);
+   // Kernel variable declarations
+   float blur_frac;
    PixelBuffer filtered_buffer = PixelBuffer(*p);
    int mask_rows_ = bounds;
-   int mask_cols_ = bounds;
-   // std::vector<std::vector<float>> kernel = {{0.0, blur_frac, 0.0},
-   //                                          {blur_frac, blur_frac, blur_frac},
-   //                                          {0.0, blur_frac, 0.0}};
+   int mask_cols_ = bounds;                                         
    std::vector<std::vector<float> > kernel(bounds, std::vector<float>(bounds));
-   int midpoint = mask_rows_/2;
-   std::cout << "midpoint: " << midpoint << std::endl;
-   for (int r = 0; r < mask_rows_; r++) {
-    for (int c = 0; c < mask_cols_; c++) {
-      if (r == midpoint || c == midpoint) {
-        kernel[r][c] = blur_frac;
-      }
-      else {
-        kernel[r][c] = 0;
-      }
-      std::cout << " , " << kernel[r][c] << " , ";
-    }
-    std::cout << std::endl;
-   }
-   std::cout << "x,y: " << x << ", " << y << std::endl;
+   // Main loop through all the pixels in the Pixel Buffer
    for(int i = 0; i<x; i++){
      for (int j = 0; j<y; j++){
        // Get kernel values, and color from current pixel
@@ -70,6 +55,28 @@ Blur::~Blur() {}
        // set the start and end positions of the draw mask
        int x_start = i - mask_cols_/2;
        int y_start = j - mask_rows_/2;
+       int blur_count = 0;
+       // Build the kernel for each pixel in the PixelBuffer
+       for (int mask_y = 0; mask_y < mask_rows_; mask_y++) {
+        for (int mask_x = 0; mask_x < mask_cols_; mask_x++) {
+          // check if it is in the bounds of the PixelBuffer. If it is in the
+          // bounds set the kernel value to 1 and increment the blur_count
+          if ((mask_x + x_start) >= 0 && (mask_x + x_start) < window_width &&
+            (mask_y + y_start) >= 0 && (mask_y + y_start) < window_height) {
+            kernel[mask_x][mask_y] = 1;
+          blur_count++;
+          }
+          else {
+            kernel[mask_x][mask_y] = 0;
+          }
+        }
+       }
+       // Now actually loop through and apply the kernel to each pixel
+       blur_frac = 1.0/blur_count; // Amount for the kernel
+       //reset the loop variables
+       x_start = i - mask_cols_/2;
+       y_start = j - mask_rows_/2;
+       // Loop through and actually apply the blur_frac
        for (int mask_y = 0; mask_y < mask_rows_; mask_y++) {
         for (int mask_x = 0; mask_x < mask_cols_; mask_x++) {
           // set the current pixel to the display buffer that we passed in
@@ -77,12 +84,12 @@ Blur::~Blur() {}
           if ((mask_x + x_start) >= 0 && (mask_x + x_start) < window_width &&
             (mask_y + y_start) >= 0 && (mask_y + y_start) < window_height) {
             ColorData temp = p->get_pixel(x_start + mask_x, y_start + mask_y);
-            temp = temp * kernel[mask_x][mask_y];
+            // Compute the new color pixel
+            temp = temp * (kernel[mask_x][mask_y] * blur_frac);
             og_color = og_color + temp;
           }
         }
-
-     }
+       }
      filtered_buffer.set_pixel(i,j, og_color);
    }
    *p = filtered_buffer;
