06d11b59326c93b1f45b969e2752c108f095bac2
Isaac <schw1643@umn.edu>
2016-11-18 17:25:49 -0600

    Special Filter - Emboss -started working on convo based filter of emboss -need to get grayscale working corectly

--- src/special.cc
+++ src/special.cc
@@ -30,28 +30,62 @@ Special::~Special() {}
  * Member Functions
  ******************************************************************************/
  void Special::apply_filter(PixelBuffer* p){
+   // PixelBuffer height and width
    int x = p->width();
    int y = p->height();
-   PixelBuffer filtered_buffer = *p;
-   for(int i = 0; i<x; i++){
-     for (int j = 0; j<y; j++){
-       ColorData pix = p->get_pixel(i,j);
-       float pix_red = pix.red();
-       float pix_blue = pix.blue();
-       float pix_green = pix.green();
-       if (pix_red > pix_blue || pix_red > pix_green){
-        pix_red = pix_red + (pix_blue * pix_green);
+   PixelBuffer filtered_buffer = PixelBuffer(*p);
+   // Calculate an odd numbered bound based off the input
+   int bounds = 3;
+   // Kernel required to create the Edge Detect effect
+   std::vector<float> kernel = {-1.0, -1.0, 0.0,
+                                -1.0, 0.0, 1.0,
+                                0.0, 1.0, 1.0};
+
+   for (int i = 0; i < x; i++) {
+    for (int j = 0; j < y; j++) {
+      // Variables for augmenting the kernel to the PixelBuffer
+      int xm_end = i + bounds/2;
+      int ym_end = j + bounds/2;
+      // This section handles edge detection cases for the kernel
+      int kernel_pos = 0;
+      int kernel_count = 0;
+      ColorData og_color = ColorData(0,0,0);
+      for (int xm_start = i - bounds/2; xm_start <= xm_end; xm_start++) {
+        for (int ym_start = j - bounds/2; ym_start <= ym_end; ym_start++) {
+          if (xm_start >= 0 && ym_start >= 0 && xm_start < x && ym_start < y) {
+            kernel_count++;  // Counter that keeps track of valid kernel pos
+          }
         }
-       if (pix_blue > pix_red || pix_blue > pix_green){
-        pix_blue = pix_blue + (pix_red * pix_green);
+      }
+      // Create the new value that will add to 1 for the kernel
+      float kernel_val = 8.0 / (kernel_count-1);
+      //std::cout << i << ", " << j << std::endl;
+      //std::cout << kernel_val << std::endl;
+      // Actually loop through the surrounding pixels and apply the kernel
+      for (int xm_start = i-bounds/2; xm_start <= xm_end; xm_start++) {
+        for (int ym_start = j-bounds/2; ym_start <= ym_end; ym_start++) {
+          if (xm_start >= 0 && ym_start >= 0 && xm_start < x && ym_start < y) {
+            //std::cout << "(" << xm_start << ", " << ym_start << ")" << std::endl;
+            ColorData temp = p->get_pixel(xm_start, ym_start);
+            // We dont't want to change the middle kernel value
+            if (kernel_pos == 4) { 
+              temp = temp * kernel[kernel_pos];
+            }
+            else {  // Set the kernel val and the generate the new color
+              temp = temp * kernel[kernel_pos] * kernel_val;
+            }
+            og_color = og_color + temp;  // Color accumulator for actual pixel
+          }
+          kernel_pos++;  // Keep track of where we are in the kernel
         }
-       if (pix_green > pix_blue || pix_green > pix_red){
-        pix_green = pix_green + (pix_red * pix_blue);
-        }
-       ColorData new_color = ColorData(pix_red,pix_green,pix_blue);
-       filtered_buffer.set_pixel(i,j,new_color);
-     }
+      }
+      float bias = og_color.luminance();
+      ColorData gray = ColorData(bias, bias, bias);
+      og_color = og_color + gray;
+      og_color = og_color.clamped_color();
+      filtered_buffer.set_pixel(i,j, og_color);  // Set the new pixel
+    }
    }
-   *p = filtered_buffer;
+   *p = filtered_buffer;  // Set the input buffer to our working buffer
  }
 }  // namespace image_tools
