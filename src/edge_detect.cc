c7a945a1b90999a94e5133510585a77769f16eae
Isaac <schw1643@umn.edu>
2016-11-18 12:54:31 -0600

    Edge Detect Filter -completely rebuilt the loop structure from the ground up. -new loop structure fixed the weird bugs we were having -was successfully able to handle the edge cases. -should now be finalized

--- src/edge_detect.cc
+++ src/edge_detect.cc
@@ -31,44 +31,58 @@ EdgeDetect::~EdgeDetect() {}
  * Member Functions
  ******************************************************************************/
  void EdgeDetect::apply_filter(PixelBuffer* p){
+   // PixelBuffer height and width
    int x = p->width();
    int y = p->height();
    PixelBuffer filtered_buffer = PixelBuffer(*p);
-   int mask_rows_ = 3;
-   int mask_cols_ = 3;
-   std::vector<std::vector<float>> kernel = {{-1.0, -1.0, -1.0},
-                                             {-1.0, 8.0, -1.0},
-                                             {-1.0, -1.0, -1.0}};
+   // Calculate an odd numbered bound based off the input
+   int bounds = 3;
+   // Kernel required to create the Edge Detect effect
+   std::vector<float> kernel = {-1.0, -1.0, -1.0,
+                                -1.0, 8.0, -1.0,
+                                -1.0, -1.0, -1.0};
 
-   for(int i = 0; i<x; i++){
-     for (int j = 0; j<y; j++){
-       // Get kernel values, and color from current pixel
-       float new_red = 0.0;
-       float new_blue = 0.0;
-       float new_green = 0.0;
-       int window_width = p->width();
-       int window_height = p->height();
-       // set the start and end positions of the draw mask
-       int x_start = i - mask_cols_/2;
-       int y_start = j - mask_rows_/2;
-       for (int mask_y = 0; mask_y < mask_rows_; mask_y++) {
-        for (int mask_x = 0; mask_x < mask_cols_; mask_x++) {
-          if ((mask_x + x_start) >= 0 && (mask_x + x_start) < window_width &&
-            (mask_y + y_start) >= 0 && (mask_y + y_start) < window_height) {
-            std::cout << "x: " << x_start+mask_x << " y: " << y_start+mask_y << std::endl;
-            ColorData pix = p->get_pixel(i + mask_x , j + mask_y);
-            new_red = new_red + (pix.red() * kernel[mask_x][mask_y]);
-            new_blue = new_blue + (pix.blue() * kernel[mask_x][mask_y]);
-            new_green = new_green + (pix.green() * kernel[mask_x][mask_y]);
+   for (int i = 0; i < x; i++) {
+    for (int j = 0; j < y; j++) {
+      // Variables for augmenting the kernel to the PixelBuffer
+      int xm_end = i + bounds/2;
+      int ym_end = j + bounds/2;
+      // This section handles edge detection cases for the kernel
+      int kernel_pos = 0;
+      int kernel_count = 0;
+      ColorData og_color = ColorData(0,0,0);
+      for (int xm_start = i - bounds/2; xm_start <= xm_end; xm_start++) {
+        for (int ym_start = j - bounds/2; ym_start <= ym_end; ym_start++) {
+          if (xm_start >= 0 && ym_start >= 0 && xm_start < x && ym_start < y) {
+            kernel_count++;  // Counter that keeps track of valid kernel pos
           }
         }
-     }
-     filtered_buffer.set_pixel(i,j,ColorData(new_red,new_blue,new_green));
-     new_red = 0.0;
-     new_blue = 0.0;
-     new_green = 0.0;
+      }
+      // Create the new value that will add to 1 for the kernel
+      float kernel_val = 8.0 / (kernel_count-1);
+      //std::cout << i << ", " << j << std::endl;
+      //std::cout << kernel_val << std::endl;
+      // Actually loop through the surrounding pixels and apply the kernel
+      for (int xm_start = i-bounds/2; xm_start <= xm_end; xm_start++) {
+        for (int ym_start = j-bounds/2; ym_start <= ym_end; ym_start++) {
+          if (xm_start >= 0 && ym_start >= 0 && xm_start < x && ym_start < y) {
+            //std::cout << "(" << xm_start << ", " << ym_start << ")" << std::endl;
+            ColorData temp = p->get_pixel(xm_start, ym_start);
+            // We dont't want to change the middle kernel value
+            if (kernel_pos == 4) { 
+              temp = temp * kernel[kernel_pos];
+            }
+            else {  // Set the kernel val and the generate the new color
+              temp = temp * kernel[kernel_pos] * kernel_val;
+            }
+            og_color = og_color + temp;  // Color accumulator for actual pixel
+          }
+          kernel_pos++;  // Keep track of where we are in the kernel
+        }
+      }
+      filtered_buffer.set_pixel(i,j, og_color);  // Set the new pixel
+    }
    }
-   *p = filtered_buffer;
- }
+   *p = filtered_buffer;  // Set the input buffer to our working buffer
  }
 }  // namespace image_tools
